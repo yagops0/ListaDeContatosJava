ce78f8920e64021e8841b694d3deafa26d17e80c
yagops0 <yagosalvador34@gmail.com>
2024-01-18 01:01:54 -0300

    Começando o projeto

---
+++ listadecontatos1/src/main/java/com/yagos/ListaDeContatos1.java
@@ -0,0 +1,28 @@
+package com.yagos;
+
+/*Faça um programa Java para implementar uma lista de contatos, com os atributos
+apelido, nome e telefone. O programa principal deverá exibir um menu com as opções:
+1 – incluir contato; 2 – alterar telefone; 3 – excluir contato; 4 – consultar um contato; 5
+– listar todos os contatos; e 6 – sair. Na opção de incluir contato, o programa deve pedir
+os dados do contato e, se não existir nenhum contato com o apelido digitado, inserir o
+contato na lista; caso contrário, o programa deve exibir uma mensagem de que já existe
+um contato com o mesmo apelido. Na opção de alterar telefone, o programa deve pedir
+o apelido do contato e procurá-lo na lista e, encontrando o contato, o programa deve
+solicitar o novo telefone e alterar na lista; caso contrário, o programa deve exibir uma
+mensagem de que o contato não existe. Na opção de excluir o contato, o programa deve
+pedir o apelido do contato e procurá-lo na lista e, encontrando o contato, o programa
+deve excluí-lo da lista; caso não encontre, o programa deve exibir uma mensagem de
+que o contato não existe. Na opção de consultar um contato, o programa deve pedir o
+apelido do contato e procurá-lo na lista e, encontrando o contato, o programa deve
+exibir todos os dados do contato; caso não encontre, o programa deve exibir uma
+mensagem de que o contato não existe. Na opção de listar todos os contatos, o
+programa irá varrer a lista e exibir todos os dados de todos os contatos. Na opção de
+sair, o programa deve ser encerrado. */
+
+public class ListaDeContatos1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
